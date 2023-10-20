69ac54f91c2b0b723975b9e5b17621cc090b79c1
tiamz <tiami.abiola@gmail.com>
2023-10-20 16:50:10 +0100

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import pickle
 import sys
 from src.logger import logging
 from src.exception import CustomException
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
         try:
@@ -15,3 +16,35 @@ def save_object(file_path, obj):
                 dill.dump(obj, file_obj)
         except Exception as e:
             raise CustomException(e, sys)
+        
+
+# def evaluate_model(X_train,X_test, y_train, y_test, models):
+#      try:
+#         report = {}
+
+#         for i in range(len(list(models))):
+#              models.fit(X_train, y_train) # Train model
+#              y_train_pred = models.predict(X_train)
+#              y_test_pred = models.predict(X_test)
+#              train_model_score = r2_score(y_train, y_train_pred)
+#              test_model_score = r2_score(y_test, y_test_pred)
+#              report[list(models.key())[i]] = test_model_score
+
+#         return report
+#      except Exception as e:
+#           raise CustomException(e, sys)
+def evaluate_model(X_train, X_test, y_train, y_test, models):
+    try:
+        report = {}
+
+        for model_name, model in models.items():
+            model.fit(X_train, y_train)  # Train the model
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+            report[model_name] = test_model_score
+
+        return report
+    except Exception as e:
+        raise CustomException(e, sys)
